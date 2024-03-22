0a119bc7aee5fcacbb364207d718c1157e59bd2a
bloms11 <danny.11chike@gmail.com>
2024-03-22 12:20:17 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,37 @@ import "./App.css";
 import Starto from "./Starto";
 import Group from "./Group";
 import Engineering from "./Engineering";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Group />} />
-        <Route path="stratolift" element={<Starto />} />
-        <Route path="stablearc-engineering" element={<Engineering />} />
-        {/* <Route path="faqs" element={<Faqs />} /> */}
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Group />} />
+      <Route path="stratolift" element={<Starto />} />
+      <Route path="stablearc-engineering" element={<Engineering />} />
+    </>
+  )
+);
+const App = () => <RouterProvider router={router} />;
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Group />} />
+//         <Route path="stratolift" element={<Starto />} />
+//         <Route path="stablearc-engineering" element={<Engineering />} />
+//         {/* <Route path="faqs" element={<Faqs />} /> */}
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
 
 export default App;
